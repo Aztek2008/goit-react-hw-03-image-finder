0ec3dc2fc7856bacbe9c55ae477f5a81d6ace725
Oleksii Skopiuk <aztek2008@gmail.com>
2020-08-10 23:27:00 +0300

    Add some changes to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,42 +8,41 @@ class App extends Component {
     searchQuery: "",
     page: 1,
     error: "",
-    loading: false,
+    // loading: false,
   };
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const prevQuery = prevState.searchQuery;
-  //   const nextQuery = this.state.searchQuery;
+  componentDidUpdate(prevProps, prevState) {
+    const prevQuery = prevState.searchQuery;
+    const nextQuery = this.state.searchQuery;
 
-  //   /*DOING FETCH IF NEW DATA IN QUERY*/
-  //   prevQuery !== nextQuery && this.fetchPicturesFromApi();
-  // }
+    /*DOING FETCH IF NEW DATA IN QUERY*/
+    prevQuery !== nextQuery && this.fetchPicturesFromApi();
+  }
 
-  // fetchPicturesFromApi = () => {
-  //   const { galleryImages, searchQuery, page } = this.state;
+  fetchPicturesFromApi = () => {
+    const { searchQuery, page } = this.state;
 
-  //   pixabayApi
-  //     .fetchImagesWithQuery(searchQuery, page)
-  //     .then(
-  //       (pictures) => console.log("pictures: ", pictures)
-  //       // this.setState({
-  //       //   galleryImages: pictures
-  //       //   // .map((picture) => ({
-  //       //   //   id: picture.id,
-  //       //   //   webformatURL: picture.webformatURL,
-  //       //   //   largeImageURL: picture.largeImageURL,
-  //       //   // })),
-  //       // })
-  //     )
-  //     .catch((error) => this.setState({ error }))
-  //     .finally(() => this.setState({ loading: false }));
-  // };
+    pixabayApi
+      .fetchImagesWithQuery(searchQuery, page)
+      .then((pictures) =>
+        // console.log("pictures: ", pictures)
+        this.setState({
+          galleryImages: pictures,
+          // .map((picture) => ({
+          //   id: picture.id,
+          //   webformatURL: picture.webformatURL,
+          //   largeImageURL: picture.largeImageURL,
+          // })),
+        })
+      )
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  };
 
-  handleSubmitFromQuery = (value) => {
-    console.log("value", value);
+  handleSubmitFromQuery = (queryValue) => {
+    console.log("value", queryValue);
     this.setState({
-      galleryImages: [],
-      searchQuery: value,
+      searchQuery: queryValue,
     });
   };
 
